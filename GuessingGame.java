309be6a5b9bf3cd15d5937d4537e40c59eda93d7
Brandon Gomez <bgbrandongomez@users.noreply.github.com>
2015-11-05 08:06:48 -0500

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,17 +1,17 @@
 public class GuessingGame {
   public static void main(String[] args) {
-   int number = 1;
+   int number = random.nextInt(max - min + 1) + min; //One could do (untested) Min + (int)(Math.random() * ((Max - Min) + 1))
    
    while(true) {
     Scanner userguess = new Scanner(System.in);
     int max = 10000;
     int min = 0;
-    int guess = random.nextInt(max - min + 1) + min; //One could do (untested) Min + (int)(Math.random() * ((Max - Min) + 1))
+    int guess = userguess.nextInt();
     int tries = 1;
     
     if(guess == number) {
       System.out.println("You have succeeded in correctly guessing the number " + number + " in " + tries + " tries!");
-      guess = random.nextInt(max - min + 1) + min;
+      number = random.nextInt(max - min + 1) + min;
       tries = 1;
     } else if(guess < number) {
       tries++;
