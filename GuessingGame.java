5d6ccb0fb9733487a11d09f1e2ba66bf8c5d91f3
Brandon Gomez <bgbrandongomez@gmail.com>
2015-11-06 13:36:48 -0500

    Actually tested the code I wrote and fixed it

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,18 +1,29 @@
+import java.util.*;
+
 public class GuessingGame {
   public static void main(String[] args) {
-   int number = random.nextInt(max - min + 1) + min; //One could do (untested) Min + (int)(Math.random() * ((Max - Min) + 1))
-   
+   Random r = new Random();
+   int max = 10000;
+   int min = 0;
+   int tries = 1;
+   int number = r.nextInt(max - min + 1) + min; //One could do (untested) Min + (int)(Math.random() * ((Max - Min) + 1))
+
+   System.out.println("Guess a number between " + min + " and " + max + "!");   
+
    while(true) {
     Scanner userguess = new Scanner(System.in);
-    int max = 10000;
-    int min = 0;
     int guess = userguess.nextInt();
-    int tries = 1;
     
     if(guess == number) {
-      System.out.println("You have succeeded in correctly guessing the number " + number + " in " + tries + " tries!");
-      number = random.nextInt(max - min + 1) + min;
+      if(tries == 1) {
+      	System.out.println("You have succeeded in correctly guessing the number " + number + " in " + tries + " try!");
+      }
+      else {
+	System.out.println("You have succeeded in correctly guessing the number " + number + " in " + tries + " tries!");
+      }
+      number = r.nextInt(max - min + 1) + min;
       tries = 1;
+      System.out.println("Press Ctrl+C to quit or type in a new number to play again!");
     } else if(guess < number) {
       tries++;
       System.out.println("Sorry, the number is higher than your guess!");
